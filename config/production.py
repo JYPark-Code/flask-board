38ce5b3a379779358fbaf179a10b16afa086dc1b
Ji Yong Park <toboy93@gmail.com>
2022-10-09 16:46:04 +0900

    production updated

--- config/production.py
+++ config/production.py
@@ -1,6 +1,7 @@
 import os
-from config.default import *
 from logging.config import dictConfig
+
+from config.default import *
 from dotenv import load_dotenv
 
 load_dotenv(os.path.join(BASE_DIR, '.env'))
@@ -10,7 +11,6 @@ SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
     pw=os.getenv('DB_PASSWORD'),
     url=os.getenv('DB_HOST'),
     db=os.getenv('DB_NAME'))
-
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'Zb3\x81\xdb\xf1\xd9\xd7-Knb\x8eB\xa5\x18'
 
